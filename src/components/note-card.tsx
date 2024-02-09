72ef1d31f1ceb11964777928c0af073d101a4d2a
rogervalentim <valentim.roger33@gmail.com>
2024-02-09 14:53:17 -0300

    feat: add initial props to NoteCard component

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,14 +1,21 @@
-export function NoteCard() {
-    return (
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
-          <span className="text-sm font-medium text-slate-300">
-            há 4 dias
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel et architecto laborum quibusdam, quo tempora doloremque eum error, est minus eaque unde corrupti ipsa, id alias sint facilis magni tenetur.
-          </p>
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
 
-         <div className="absolute bottom-0 left-0 right-0  h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"  />
-      </div>
-    )
+export function NoteCard({ note }: NoteCardProps) {
+  return (
+    <button className="rounded-md text-left flex flex-col bg-slate-800  p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
+      <span className="text-sm font-medium text-slate-300">
+        {note.date.toISOString()}
+      </span>
+      <p className="text-sm leading-6 text-slate-400">
+        {note.content}
+      </p>
+
+      <div className="absolute bottom-0 left-0 right-0  h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+    </button>
+  );
 }
